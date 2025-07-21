b379826368ab767a87152b8e70d9e24187faa3e7
Virtual Sense <virtual_sense@naver.com>
2025-07-21 15:12:23 +0900

    커맨드는 수정가능
    확장된 설명도 가능~~~!!!!

---
+++ C/Basic/loop01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+	//1.초기값
+	int i = 100;
+
+	while (i >=0) {
+		printf("%d\n", i--);
+		// i--;
+		// i -= 1;
+		// i = i - 1;
+	}
+	return 0;
+}
